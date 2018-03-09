b31a081fab6f546ee92c7ab8879ebf361feef71b
NarekCS <33220145+NarekCS@users.noreply.github.com>
2018-03-10 00:50:41 +0400

    Cart

---
+++ Controllers/CartController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using Microsoft.AspNetCore.Http;
+using SportsStore.Infrastructure;
+using Newtonsoft.Json;
+using SportsStore.Models.ViewModels;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+         }
+
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToActionResult RemoveFromCart(int prodcutId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == prodcutId);
+
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return cart;
+        }
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,8 @@ namespace SportsStore
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +42,7 @@ namespace SportsStore
             }
             app.UseStatusCodePages();
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
