6ec526c6f5e02be3debfd9f9de6e5a90edf19901
NarekCS <33220145+NarekCS@users.noreply.github.com>
2018-03-12 15:10:15 +0400

    Order

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Checkout()
+        {
+            return View(new Order());
+        }
+    }
+}
