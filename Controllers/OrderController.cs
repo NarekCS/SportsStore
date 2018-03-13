f4d57f5755b56c1d8e0443d5fc06f856fe74e706
NarekCS <33220145+NarekCS@users.noreply.github.com>
2018-03-14 02:45:22 +0400

    EFOrderRepository

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -11,10 +11,41 @@ namespace SportsStore.Controllers
 {
     public class OrderController : Controller
     {
-        // GET: /<controller>/
+        private IOrderRepository repository;
+        private Cart cart;
+
+        public OrderController(IOrderRepository repoService, Cart cartService)
+        {
+            repository = repoService;
+            cart = cartService;
+        }
         public IActionResult Checkout()
         {
             return View(new Order());
+        }
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                repository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        public ViewResult Completed()
+        {
+            cart.Clear();
+            return View();
         }
     }
 }
