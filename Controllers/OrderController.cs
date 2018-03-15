5cd520edb84859e69852809a76414dd887a20ad2
NarekCS <33220145+NarekCS@users.noreply.github.com>
2018-03-16 02:45:43 +0400

    Order ship

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -19,6 +19,21 @@ namespace SportsStore.Controllers
             repository = repoService;
             cart = cartService;
         }
+
+        public ViewResult List() => View(repository.Orders.Where(o => !o.Shipped));
+
+        [HttpPost]
+        public IActionResult MarkShipped(int orderID)
+        {
+            Order order = repository.Orders.FirstOrDefault(o => o.OrderID == orderID);
+            if (order != null)
+            {
+                order.Shipped = true;
+                repository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
+
         public IActionResult Checkout()
         {
             return View(new Order());
--- Migrations/20180315200026_ShippedOrder.Designer.cs
+++ Migrations/20180315200026_ShippedOrder.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SportsStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180315200026_ShippedOrder")]
+    partial class ShippedOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
