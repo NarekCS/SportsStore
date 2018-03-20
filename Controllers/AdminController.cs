ecc765540d46f5ea429b7b922e21016e1d499554
NarekCS <33220145+NarekCS@users.noreply.github.com>
2018-03-20 18:20:09 +0400

    Edit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        
+        public IActionResult Index()
+        {
+            return View(repository.Products);
+        }
+        public ViewResult Edit(int productId)
+        {
+            return View(repository.Products.FirstOrDefault(p => p.ProductID == productId));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = $"{product.Name} has been saved";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+    }
+}
